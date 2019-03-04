b8c8645982f68ecbb00a7e908c8a5b7f7f5c4165
RatarovDaniil <38246090+RatarovDaniil@users.noreply.github.com>
2019-03-04 14:04:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ function isUserAuthenticated(req, res, next) {
     if (req.user) {
         return next();
     } else {
-        res.redirect('https://login.cern.ch/adfs/ls/?wa=wsignin1.0');
+        return res.redirect('/callback');
     }
 }
 
