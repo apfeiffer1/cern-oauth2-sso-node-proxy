b49c6e65db4079a13b1a16f3ade729e646de19a1
RatarovDaniil <38246090+RatarovDaniil@users.noreply.github.com>
2019-03-04 14:41:34 +0100

    Update app.js

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.get('/logout', function(req, res, next) {
                 return next(err);
             } else {
                 res.clearCookie('connect.sid');
-                return res.redirect('https://login.cern.ch/adfs/ls/?wa=wsignout1.0');
+                res.redirect('https://login.cern.ch/adfs/ls/?wa=wsignout1.0&next=http://home.cern');
             }
         });
     } else {
