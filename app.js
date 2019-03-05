bbe919df298a5a683d1c657460aa1ae5773034df
RatarovDaniil <daniil.ratarov@gmail.com>
2019-03-05 19:36:20 +0100

    update

--- app.js
+++ app.js
@@ -80,14 +80,11 @@ app.get(
 
         request('https://test-cms-career.web.cern.ch/api/cms_hr/' + user.id, function (error, response, body) {
             if (!error && response.statusCode == 200) {
-                console.log('Body:', body)
                 const cms_id = body['cms_id'];
             } else {
                 const cms_id = undefined;
             }
-        });
-        console.log('CMS_ID:', cms_id)
-
+        })
         if (cms_id !== '' && cms_id !== null && cms_id !== undefined) {
             res.redirect('/user?user=' + cms_id);
         } else {
